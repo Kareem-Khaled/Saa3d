ee071bc5ecc357aa49053d3fcfacf1fb538334a4
kareemkhaled1 <kareemkhaled143@gmail.com>
2022-06-25 21:48:45 +0200

    edit notifications

--- public/javascripts/sockets/socket.js
+++ public/javascripts/sockets/socket.js
@@ -1,14 +1,7 @@
 const socket = io();
 
 let notifyBtn = document.getElementsByClassName('notify');
-let notifyMe = document.getElementById('notifyMe');
 const notfNum = document.getElementsByClassName('notfNum')[0];
-
-// if(notifyBtn)
-//     notifyBtn = notifyBtn[0];
-
-if(notifyMe)
-    notifyMe = notifyMe.value;
 
 socket.on('connect', () =>{
     currentUserId = document.getElementById('currentUser').value;
@@ -16,20 +9,10 @@ socket.on('connect', () =>{
 })
 
 for (let btn of notifyBtn) {
-    if(btn.textContent == 'Comment'){
-        btn.onclick = (e) =>{
-            socket.emit('newNotf', notifyMe);
-        };
-    }
-    else{
         btn.onclick = (e) =>{
             socket.emit('newNotf', btn.value);
         };
-    }
 }    
-// notifyBtn.onclick = (e) =>{
-//     socket.emit('newNotf', notifyMe);
-// }
 
 socket.on('addNotf', () => {
     notfNum.innerHTML = Number(notfNum.innerHTML) + 1;  
